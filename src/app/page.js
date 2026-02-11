e9ad952a44b72ba7207b481fc8cf8ff47664013b
oneworld foodinc <jhoncedrick.fuentes@gmail.com>
2026-02-11 15:56:32 +0800

    updated files

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,17 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import AboutSection from './components/AboutSection'
-import InfoSections from './components/InfoSections'
-import BrandsSection from './components/BrandsSections' 
-import HistorySection from './components/HistorySection'
-import MapSection from './components/MapSection'
-import Footer from './components/Footer'
-import ScrollToTop from './components/ScrolltoTop' 
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import AboutSection from '@/components/AboutSection'
+import InfoSections from '@/components/InfoSections'
+import BrandsSection from '@/components/BrandSection'  
+import HistorySection from '@/components/HistorySection'
+import MapSection from '@/components/MapSection'
+import Footer from '@/components/Footer'
+import ScrollToTop from '@/components/ScrollToTop'
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Header />
-      <Hero />
+      <Header />   
+      <Hero />     
       <AboutSection />
       <InfoSections />
       <BrandsSection />
