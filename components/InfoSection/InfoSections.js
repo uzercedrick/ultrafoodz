505e947105baaaeebfd1b8aedbf373c3a97d9aab
oneworld foodinc <jhoncedrick.fuentes@gmail.com>
2026-02-14 11:31:25 +0800

    updated component folder

--- components/InfoSection/InfoSections.js
+++ components/InfoSection/InfoSections.js
@@ -11,18 +11,14 @@ export default function Mission() {
             <h2 className={styles.heading}>VISION</h2>
             <p className={styles.text}>Description Text - Your vision statement goes here.</p>
           </div>
-          <div className={styles.imagePlaceholder}>
-            
-          </div>
+          <div className={styles.imagePlaceholder}></div>
         </div>
       </section>
 
       {/* Mission Section */}
       <section className={`${styles.section} ${styles.sectionWhite}`}>
         <div className={styles.container}>
-          <div className={styles.imagePlaceholder}>
-            
-          </div>
+          <div className={styles.imagePlaceholder}></div>
           <div className={styles.slideInRight}>
             <h2 className={styles.heading}>MISSION</h2>
             <p className={styles.text}>What we do - Your mission statement.</p>
